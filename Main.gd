ae833be82c95c63ce581c022ad42a69f826adc07
Bastiaan Olij <mux213@gmail.com>
2022-09-30 15:36:36 +1000

    Add Android support and setup

--- Main.gd
+++ Main.gd
@@ -12,8 +12,13 @@ func _ready():
 
 		var vp : Viewport = get_viewport()
 		vp.use_xr = true
+		
+		# vp.vrs_mode = Viewport.VRS_XR
 	else:
 		print("OpenXR not initialised, please check if your headset is connected")
 
 func _on_right_hand_button_pressed(p_name):
 	print("Pressed " + str(p_name))
+
+func _process(delta):
+	$XROrigin3D/XRCamera3D/FPS.text = "FPS: %d" % Engine.get_frames_per_second()
